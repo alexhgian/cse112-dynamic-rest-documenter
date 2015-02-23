b2418a29bcb49821ed8e2b2818743e95fb9143b9
alex gian <alexhgian@gmail.com>
2015-02-23 01:42:51 -0800

    Added Generator

--- README.md
+++
@@ -1,5 +0,0 @@
-# Dynamic REST API Documenter
-This app generates documentation populated using a JSON.
-I'll probably create a JSON Editor in the future.
-
-http://alexhgian.github.io/cse112-dynamic-rest-documenter
--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 var app = angular.module('plunker', ['ui.bootstrap']);
 
+app.directive('jsonText', function() {
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: 'ngModel', // get a hold of NgModelController
+    link: function(scope, element, attrs, ngModelCtrl) {
+      function fromUser(text) {
+        // Beware: trim() is not available in old browsers
+        if (!text || text.trim() === '')
+          return {}
+        else
+          // TODO catch SyntaxError, and set validation error..
+          return angular.fromJson(text);
+      }
+
+      function toUser(object) {
+          // better than JSON.stringify(), because it formats + filters $$hashKey etc.
+          return angular.toJson(object, true);
+      }
+      
+      // push() if faster than unshift(), and avail. in IE8 and earlier (unshift isn't)
+      ngModelCtrl.$parsers.push(fromUser);
+      ngModelCtrl.$formatters.push(toUser);
+      
+      // $watch(attrs.ngModel) wouldn't work if this directive created a new scope;
+      // see http://stackoverflow.com/questions/14693052/watch-ngmodel-from-inside-directive-using-isolate-scope how to do it then
+      scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+        if (newValue != oldValue) {
+          ngModelCtrl.$setViewValue(toUser(newValue));
+          // TODO avoid this causing the focus of the input to be lost..
+          ngModelCtrl.$render();
+        }
+      }, true); // MUST use objectEquality (true) here, for some reason..
+    }
+  };  
+});
+
 app.controller('MainCtrl', function($scope, $http) {
 
    $scope.message = "Hello";
 
-   $scope.pages = ["businessPage", "employeePage", "checkinPage", "formsPage", "confirmPage", "authPage"];
-   //$scope.pageSelect = $scope.pages[1]; //Page at load, good for debugging
+   $scope.pages = ["businessPage", "employeePage", "checkinPage", "formsPage", "confirmPage", "authPage", "genPage"];
+   $scope.pageSelect = $scope.pages[1]; //Page at load, good for debugging
 });
 
 //Business API
@@ -307,6 +343,20 @@ app.controller('authCtrl', function($scope, $http) {
       reqParam: [],
       response: {},
       resParam: []
+   }];
+
+});
+
+//Generator
+app.controller('genCtrl', function($scope, $http) {
+   $scope.resource = [{
+      header: "Get Data from generator",
+      method: "GET",
+      uri: "/api/generator/:id",
+      request: { "key" : "value" },
+      reqParam: [["var","type","description"]],
+      response: { "key" : "value" },
+      resParam: [["var","type","description"]]
    }];
 
 });
